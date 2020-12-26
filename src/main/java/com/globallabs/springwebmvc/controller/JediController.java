80edef4f09ffc5e13a2a511f75ccb19b8c8cad57
DeniseLuiz <denisesantos.luiz@usp.br>
2020-12-26 20:10:16 -0300

    Update JediController.java

--- src/main/java/com/globallabs/springwebmvc/controller/JediController.java
+++ src/main/java/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,11 +1,12 @@
 package com.globallabs.springwebmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
-class JediController {
-    @GetMapping("jedi")
+class
+JediController {
+    @GetMapping("/jedi")
     public String jedi(){
         return "jedi";
     }
